6d89ebbb3011e0a6b4713ab15d9f25d7e50555e3
Chisom-cell <iheuwachisom5@gmail.com>
2026-08-29 18:25:43 +0100

    added URL patterns for about and contact

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,8 @@ from portfolio import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
+
+
